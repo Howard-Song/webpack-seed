c0b81e3d04bd4e620addc5d31043e1c6e00bb2f0
yooRabbit <bandolerolee@gmail.com>
2020-04-02 16:47:58 +0800

    热加载创建、打包区分production环境、代码打包后混淆压缩

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -1,23 +1,34 @@
 /*
  * @Author: yoo
  * @Date: 2020-04-02 10:36:38
- * @LastEditTime: 2020-04-02 16:01:24
+ * @LastEditTime: 2020-04-02 16:45:21
  * @LastEditors: yoo
  */
 const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 module.exports = {
-  devtool: 'eval-source-map',
+  devtool: 'null',
   entry: __dirname + "/app/main.js",
   output: {
     path: __dirname + "/build",
-    filename: "bundle.js"
+    filename: "bundle-[hash].js"
+  },
+  optimization: {
+    minimizer: [
+      new TerserPlugin({
+        cache: true, // 开启缓存
+        parallel: true, // 支持多进程
+        sourceMap: true,
+      }),
+    ]
   },
   devServer: {
     contentBase: "./public",
     historyApiFallback: true,
     inline: true,
-    hot: true
   },
   module: {
     rules: [
@@ -59,6 +70,8 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: __dirname + "/app/index.html"//new 一个这个插件的实例，并传入相关的参数
     }),
-    new webpack.HotModuleReplacementPlugin()
+    new webpack.optimize.OccurrenceOrderPlugin(),
+    new ExtractTextPlugin("styles.css"),
+    new CleanWebpackPlugin()
   ],
 }
